2d6f79ca6d2059124af86bfd22a5e37a595811ba
Jonas Bang <jonasbang@hotmail.dk>
2022-05-03 09:16:00 +0200

    Moved init folder

--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,33 @@ import Head from "next/head"
 import Link from "next/link"
 
 export default function Home() {
-    const [markdown, setMarkdown] = useState("");
-    const [isOpen, setIsOpen] = useState(null);
-    const [isDraw, setIsDraw] = useState(null);
-    const [isLose, setIsLose] = useState(null);
-    const [isHovered, setIsHovered] = useState(null);
+    const [markdown, setMarkdown] = useState("")
+    const [isOpen, setIsOpen] = useState(null)
+    const [isDraw, setIsDraw] = useState(null)
+    const [isLose, setIsLose] = useState(null)
+    const [isHovered, setIsHovered] = useState(null)
 
-    var HUMAN = -1;
-    var COMP = +1;
+    var HUMAN = -1
+    var COMP = +1
 
     var board = [
         [0, 0, 0],
         [0, 0, 0],
         [0, 0, 0],
-    ];
+    ]
 
     const evalute = (state) => {
-        var score = 0;
+        var score = 0
 
         if (gameOver(state, COMP)) {
-            score = +1;
-        }
-        else if (gameOver(state, HUMAN)) {
-            score = -1;
+            score = +1
+        } else if (gameOver(state, HUMAN)) {
+            score = -1
         } else {
-            score = 0;
+            score = 0
         }
 
-        return score;
+        return score
     }
 
     const gameOver = (state, player) => {
@@ -45,195 +44,215 @@ export default function Home() {
             [state[0][2], state[1][2], state[2][2]],
             [state[0][0], state[1][1], state[2][2]],
             [state[2][0], state[1][1], state[0][2]],
-        ];
+        ]
 
         for (var i = 0; i < 8; i++) {
-            var line = win_state[i];
-            var filled = 0;
+            var line = win_state[i]
+            var filled = 0
             for (var j = 0; j < 3; j++) {
-                if (line[j] == player)
-                    filled++;
+                if (line[j] == player) filled++
             }
-            if (filled == 3)
-                return true;
+            if (filled == 3) return true
         }
-        return false;
+        return false
     }
 
     const gameOverAll = (state) => {
-        return gameOver(state, HUMAN) || gameOver(state, COMP);
+        return gameOver(state, HUMAN) || gameOver(state, COMP)
     }
 
     const emptyCells = (state) => {
-        var cells = [];
+        var cells = []
         for (var x = 0; x < 3; x++) {
             for (var y = 0; y < 3; y++) {
-                if (state[x][y] == 0)
-                    cells.push([x, y]);
+                if (state[x][y] == 0) cells.push([x, y])
             }
         }
 
-        return cells;
+        return cells
     }
 
     const validMove = (x, y) => {
-        var empties = emptyCells(board);
+        var empties = emptyCells(board)
         try {
             if (board[x][y] == 0) {
-                return true;
-            }
-            else {
-                return false;
+                return true
+            } else {
+                return false
             }
         } catch (e) {
-            return false;
+            return false
         }
     }
 
     const setMove = (x, y, player) => {
         if (validMove(x, y)) {
-            board[x][y] = player;
-            return true;
-        }
-        else {
-            return false;
+            board[x][y] = player
+            return true
+        } else {
+            return false
         }
     }
 
     const minimaxAlgorithm = (state, depth, player) => {
-        var best;
+        var best
 
         if (player == COMP) {
-            best = [-1, -1, -1000];
-        }
-        else {
-            best = [-1, -1, +1000];
+            best = [-1, -1, -1000]
+        } else {
+            best = [-1, -1, +1000]
         }
 
         if (depth == 0 || gameOverAll(state)) {
-            var score = evalute(state);
-            return [-1, -1, score];
+            var score = evalute(state)
+            return [-1, -1, score]
         }
 
         emptyCells(state).forEach(function (cell) {
-            var x = cell[0];
-            var y = cell[1];
-            state[x][y] = player;
-            var score = minimaxAlgorithm(state, depth - 1, -player);
-            state[x][y] = 0;
-            score[0] = x;
-            score[1] = y;
+            var x = cell[0]
+            var y = cell[1]
+            state[x][y] = player
+            var score = minimaxAlgorithm(state, depth - 1, -player)
+            state[x][y] = 0
+            score[0] = x
+            score[1] = y
 
             if (player == COMP) {
-                if (score[2] > best[2])
-                    best = score;
+                if (score[2] > best[2]) best = score
+            } else {
+                if (score[2] < best[2]) best = score
             }
-            else {
-                if (score[2] < best[2])
-                    best = score;
-            }
-        });
+        })
 
-        return best;
+        return best
     }
 
     const aiTurn = () => {
-        var x, y;
-        var move;
-        var cell;
+        var x, y
+        var move
+        var cell
 
         if (emptyCells(board).length == 9) {
-            x = parseInt(Math.random() * 3);
-            y = parseInt(Math.random() * 3);
-        }
-        else {
-            move = minimaxAlgorithm(board, emptyCells(board).length, COMP);
-            x = move[0];
-            y = move[1];
+            x = parseInt(Math.random() * 3)
+            y = parseInt(Math.random() * 3)
+        } else {
+            move = minimaxAlgorithm(board, emptyCells(board).length, COMP)
+            x = move[0]
+            y = move[1]
         }
 
         if (setMove(x, y, COMP)) {
-            cell = document.getElementById(String(x) + String(y));
-            cell.innerHTML = "O";
+            cell = document.getElementById(String(x) + String(y))
+            cell.innerHTML = "O"
         }
     }
 
     const clickedCell = (e) => {
-        var button = document.getElementById("bnt-restart");
-        button.disabled = true;
-        var conditionToContinue = gameOverAll(board) == false && emptyCells(board).length > 0;
+        var button = document.getElementById("bnt-restart")
+        button.disabled = true
+        var conditionToContinue = gameOverAll(board) == false && emptyCells(board).length > 0
 
         if (conditionToContinue == true) {
-            var x = e.target.id.split("")[0];
-            var y = e.target.id.split("")[1];
-            var move = setMove(x, y, HUMAN);
+            var x = e.target.id.split("")[0]
+            var y = e.target.id.split("")[1]
+            var move = setMove(x, y, HUMAN)
             if (move == true) {
-                e.target.innerHTML = "X";
-                if (conditionToContinue)
-                    aiTurn();
+                e.target.innerHTML = "X"
+                if (conditionToContinue) aiTurn()
             }
         }
         if (gameOver(board, COMP)) {
-            var lines;
-            var cell;
+            var lines
+            var cell
 
             if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
-                lines = [[0, 0], [0, 1], [0, 2]];
+                lines = [
+                    [0, 0],
+                    [0, 1],
+                    [0, 2],
+                ]
             else if (board[1][0] == 1 && board[1][1] == 1 && board[1][2] == 1)
-                lines = [[1, 0], [1, 1], [1, 2]];
+                lines = [
+                    [1, 0],
+                    [1, 1],
+                    [1, 2],
+                ]
             else if (board[2][0] == 1 && board[2][1] == 1 && board[2][2] == 1)
-                lines = [[2, 0], [2, 1], [2, 2]];
+                lines = [
+                    [2, 0],
+                    [2, 1],
+                    [2, 2],
+                ]
             else if (board[0][0] == 1 && board[1][0] == 1 && board[2][0] == 1)
-                lines = [[0, 0], [1, 0], [2, 0]];
+                lines = [
+                    [0, 0],
+                    [1, 0],
+                    [2, 0],
+                ]
             else if (board[0][1] == 1 && board[1][1] == 1 && board[2][1] == 1)
-                lines = [[0, 1], [1, 1], [2, 1]];
+                lines = [
+                    [0, 1],
+                    [1, 1],
+                    [2, 1],
+                ]
             else if (board[0][2] == 1 && board[1][2] == 1 && board[2][2] == 1)
-                lines = [[0, 2], [1, 2], [2, 2]];
+                lines = [
+                    [0, 2],
+                    [1, 2],
+                    [2, 2],
+                ]
             else if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1)
-                lines = [[0, 0], [1, 1], [2, 2]];
+                lines = [
+                    [0, 0],
+                    [1, 1],
+                    [2, 2],
+                ]
             else if (board[2][0] == 1 && board[1][1] == 1 && board[0][2] == 1)
-                lines = [[2, 0], [1, 1], [0, 2]];
+                lines = [
+                    [2, 0],
+                    [1, 1],
+                    [0, 2],
+                ]
 
             for (var i = 0; i < lines.length; i++) {
-                cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]));
-                cell.style.color = "red";
+                cell = document.getElementById(String(lines[i][0]) + String(lines[i][1]))
+                cell.style.color = "red"
             }
 
-            setIsLose(true);
-            setIsOpen(true);
+            setIsLose(true)
+            setIsOpen(true)
         }
         if (emptyCells(board).length == 0 && !gameOverAll(board)) {
-            setIsDraw(true);
-            setIsOpen(true);
+            setIsDraw(true)
+            setIsOpen(true)
         }
         if (gameOverAll(board) == true || emptyCells(board).length == 0) {
-            button.value = "Restart";
-            button.disabled = false;
+            button.value = "Restart"
+            button.disabled = false
         }
     }
 
     const restartGame = (e) => {
         if (e.target.value == "Start AI") {
-            aiTurn();
-            e.target.disabled = true;
-        }
-        else if (e.target.value == "Restart") {
-            var htmlBoard;
+            aiTurn()
+            e.target.disabled = true
+        } else if (e.target.value == "Restart") {
+            var htmlBoard
 
             for (var x = 0; x < 3; x++) {
                 for (var y = 0; y < 3; y++) {
-                    board[x][y] = 0;
-                    htmlBoard = document.getElementById(String(x) + String(y));
-                    htmlBoard.style.color = "#444";
-                    htmlBoard.innerHTML = "";
+                    board[x][y] = 0
+                    htmlBoard = document.getElementById(String(x) + String(y))
+                    htmlBoard.style.color = "#444"
+                    htmlBoard.innerHTML = ""
                 }
             }
-            e.target.value = "Start AI";
+            e.target.value = "Start AI"
         }
     }
 
     const restartGameRefresh = (e) => {
-        window.location.reload(true);
+        window.location.reload(true)
     }
 
     useEffect(() => {
@@ -255,31 +274,37 @@ export default function Home() {
                         <table id="tab-tic-tac-toe" cellSpacing="0">
                             <tbody>
                                 <tr>
-                                    <td onClick={((e) => clickedCell(e))} id="00" />
-                                    <td onClick={((e) => clickedCell(e))} id="01" />
-                                    <td onClick={((e) => clickedCell(e))} id="02" />
+                                    <td onClick={(e) => clickedCell(e)} id="00" />
+                                    <td onClick={(e) => clickedCell(e)} id="01" />
+                                    <td onClick={(e) => clickedCell(e)} id="02" />
                                 </tr>
                                 <tr>
-                                    <td onClick={((e) => clickedCell(e))} id="10" />
-                                    <td onClick={((e) => clickedCell(e))} id="11" />
-                                    <td onClick={((e) => clickedCell(e))} id="12" />
+                                    <td onClick={(e) => clickedCell(e)} id="10" />
+                                    <td onClick={(e) => clickedCell(e)} id="11" />
+                                    <td onClick={(e) => clickedCell(e)} id="12" />
                                 </tr>
                                 <tr>
-                                    <td onClick={((e) => clickedCell(e))} id="20" />
-                                    <td onClick={((e) => clickedCell(e))} id="21" />
-                                    <td onClick={((e) => clickedCell(e))} id="22" />
+                                    <td onClick={(e) => clickedCell(e)} id="20" />
+                                    <td onClick={(e) => clickedCell(e)} id="21" />
+                                    <td onClick={(e) => clickedCell(e)} id="22" />
                                 </tr>
                             </tbody>
                         </table>
                         <div className="control-wrapper">
-                            <input type="button" value="Start AI" id="bnt-restart" onClick={((e) => restartGame(e))} />
+                            <input type="button" value="Start AI" id="bnt-restart" onClick={(e) => restartGame(e)} />
+                            <input type="button" value="How it works?" id="bnt-how" onClick={(e) => restartGame(e)} />
                         </div>
                     </div>
                     <div className="description">
                         <div className="desc-inner">
                             <div className="markdown-body">
                                 <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
-                                <p className="link">Check out the repository <Link href="https://github.com/hrbang/Minimax-algorithm-WEB"><a>Here</a></Link></p>
+                                <p className="link">
+                                    Check out the repository{" "}
+                                    <Link href="https://github.com/hrbang/Minimax-algorithm-WEB">
+                                        <a>Here</a>
+                                    </Link>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -308,28 +333,17 @@ export default function Home() {
                                 </div>
                                 <div className="body-text">
                                     <h2 className="text">
-                                        {isLose == true && (
-                                            "The AI wins, you lost"
-                                        )}
-                                        {isDraw == true && (
-                                            "The game is draw!"
-                                        )}
+                                        {isLose == true && "The AI wins, you lost"}
+                                        {isDraw == true && "The game is draw!"}
                                     </h2>
                                 </div>
                                 <div className="modal-restart">
-                                    <button type="button" className="restart" onClick={((e) => restartGameRefresh(e))}>Restart</button>
+                                    <button type="button" className="restart" onClick={(e) => restartGameRefresh(e)}>
+                                        Restart
+                                    </button>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className={isHovered == true ? "copyright is-hovered" : "copyright"} onMouseEnter={(e) => setIsHovered(true)} onMouseLeave={(e) => setIsHovered(false)}>
-                    <div className="copyright-icon">
-                        <svg width="20" height="20" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.25 13.5C10.7018 13.5 13.5 10.7018 13.5 7.25C13.5 3.79822 10.7018 1 7.25 1C3.79822 1 1 3.79822 1 7.25C1 10.7018 3.79822 13.5 7.25 13.5Z" stroke="#181616" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            <path d="M9.25 6C9.25 6 8.5 5 7.25 5C6 5 5 6 5 7.25C5 8.5 6 9.5 7.25 9.5C8.5 9.5 9.25 8.5 9.25 8.5" stroke="#181616" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <p><Link href="https://jonas-bang.com"><a>Jonas Bang 2022</a></Link> - Under the MIT license</p>
                     </div>
                 </div>
             </div>
